d6bdb5e856d04f16ad948d71553b3861c3a535ba
Arisly <93330293+Arisly@users.noreply.github.com>
2021-11-05 22:18:00 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '18ede413c6e145bdbf4a64cbd8185730',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Aris',
+  domain: 'https://arislee.notion.site/Share-18ede413c6e145bdbf4a64cbd8185730',
+  author: 'Aris Lee',
 
   // open graph metadata (optional)
   description: 'Example site description',
